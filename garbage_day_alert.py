b45bc09dcde2275422b6ebe6d481e1e8af6ea639
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2021-03-08 11:13:14 +0900

    通知に曜日情報を出すように修正

--- garbage_day_alert.py
+++ garbage_day_alert.py
@@ -9,15 +9,14 @@ headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
 url = "https://notify-api.line.me/api/notify"
 
 # ゴミ出し曜日情報
-mon, tue, wed, thu, fri, sat, sun = range(7)
 DAILY_EVENT = {
-    mon: "燃やすゴミ、燃えないゴミ、スプレー缶、乾電池",
-    tue: "古紙・古布・段ボール",
-    wed: None,
-    thu: "プラスチック製容器包装",
-    fri: "燃やすゴミ、燃えないゴミ、スプレー缶、乾電池",
-    sat: "缶・ビン・ペットボトル、小さな金属類",
-    sun: None,
+    "月": "燃やすゴミ、燃えないゴミ、スプレー缶、乾電池",
+    "火": "古紙・古布・段ボール",
+    "水": None,
+    "木": "プラスチック製容器包装",
+    "金": "燃やすゴミ、燃えないゴミ、スプレー缶、乾電池",
+    "土": "缶・ビン・ペットボトル、小さな金属類",
+    "日": None,
 }
 
 
@@ -27,8 +26,8 @@ def garbage_day():
     # そのため、JST(日本時間) で AM7:30 を指定したい場合は、
     # Heroku 側で 前日の PM22:30(UTC) を指定しますが、
     # その際に曜日が1 日ずれるので、その分を調整 (+1日) します。
-    event = DAILY_EVENT[weekday+1]
-    event and send(f"\n今日は{event}の日です！")
+    day, event = list(DAILY_EVENT.items())[weekday+1]
+    event and send(f"\n{day}曜日は{event}の日です！")
 
 
 def send(message):
